04145126e87a675545f926ba62d63ff13c1c44b9
ariqamuharam <ariqamuharam@gmail.com>
2025-08-12 11:04:38 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,11 @@ mkdir -p /var/lib/certbot
 mkdir -p /var/log/nginx
 touch /var/log/nginx/access.log /var/log/nginx/error.log
 
-# domains.json
-[ -f /etc/nginx/domains.json ] || echo '{}' > /etc/nginx/domains.json
-addgroup -S nginx 2>/dev/null || true
-chgrp nginx /etc/nginx/domains.json || true
-chmod 664 /etc/nginx/domains.json || true
+# # domains.json
+# [ -f /etc/nginx/domains.json ] || echo '{}' > /etc/nginx/domains.json
+# addgroup -S nginx 2>/dev/null || true
+# chgrp nginx /etc/nginx/domains.json || true
+# chmod 664 /etc/nginx/domains.json || true
 
 # Dummy cert fallback
 mkdir -p /etc/nginx/ssl
